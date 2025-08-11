683e70e5ccd3828b9b21b696921d47947f86ba19
Frank Sanchez <franksanchez.1619@gmail.com>
2025-08-11 01:06:19 -0500

    configuracion en Cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // o el dominio del frontend en producción
+    credentials: true, // si vas a enviar cookies o headers de autenticación
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
